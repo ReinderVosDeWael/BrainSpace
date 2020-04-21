6f67000129f07feb05f31262bf34dc3ec77bd96d
Reinder Vos de Wael <reinder.vosdewael@gmail.com>
2020-04-21 13:56:30 -0700

    Removed deprecated functions.

--- matlab/plot_data/volume_viewer/@volume_viewer/overlay_colormap.m
+++
@@ -1,9 +0,0 @@
-function overlay_colormap(obj,cmap)
-% Set the overlay colormaps. 
-
-% Set axis color
-set([obj.handles.axes4,obj.handles.axes5,obj.handles.axes6], ...
-    'ColorMap'          , cmap              ); % Listener is not recursive. 
-
-drawnow
-end
